02b8c97995fdc57b9022dbcaf6d7c8b99a053afd
Mikhail Polyakov <mikhail.a.polyakov@gmail.com>
2025-08-21 10:22:46 +0400

    added get_class_weights func

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import torch
 from transformers import set_seed
+from sklearn.utils.class_weight import compute_class_weight
 
 def set_global_seed(seed: int = 42):
     """
@@ -17,3 +18,16 @@ def set_global_seed(seed: int = 42):
 def save_model(trainer, path):
     os.makedirs(path, exist_ok=True)
     trainer.save_model(path)
+
+
+def get_class_weights(labels):
+
+    labels_np = np.array(labels)
+    unique_classes = np.unique(labels_np)
+
+    class_weights_np = compute_class_weight(class_weight="balanced", classes=unique_classes, y=labels_np)
+    class_weights = torch.tensor(class_weights_np, dtype=torch.float)
+
+    return class_weights
+
+
