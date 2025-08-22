85eea42eec851db53fe901f3ca469830be1f7b0d
Mikhail Polyakov <mikhail.a.polyakov@gmail.com>
2025-08-22 09:46:24 +0400

    created custom_compute_loss func

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import numpy as np
 import torch
 from transformers import set_seed
 from sklearn.utils.class_weight import compute_class_weight
+from torch.nn import CrossEntropyLoss
+
 
 def set_global_seed(seed: int = 42):
     """
@@ -15,19 +17,29 @@ def set_global_seed(seed: int = 42):
     torch.cuda.manual_seed_all(seed)   # PyTorch GPU
     set_seed(seed)                     
 
+
 def save_model(trainer, path):
     os.makedirs(path, exist_ok=True)
     trainer.save_model(path)
 
 
 def get_class_weights(labels):
-
-    labels_np = np.array(labels)
-    unique_classes = np.unique(labels_np)
-
-    class_weights_np = compute_class_weight(class_weight="balanced", classes=unique_classes, y=labels_np)
+    
+    class_weights_np = compute_class_weight(class_weight="balanced", 
+                                            classes=np.unique(labels), 
+                                            y=labels)
     class_weights = torch.tensor(class_weights_np, dtype=torch.float)
 
     return class_weights
 
 
+def custom_compute_loss(model, inputs, class_weights, return_outputs=False):
+
+    labels = inputs.get("labels").to(class_weights.device)
+    outputs = model(**inputs)
+
+    logits = outputs.get("logits")
+    loss_fct = CrossEntropyLoss(weight=class_weights.to(logits.device))
+    
+    loss = loss_fct(logits.view(-1, logits.size(-1)), labels.view(-1))
+    return (loss, outputs) if return_outputs else loss
